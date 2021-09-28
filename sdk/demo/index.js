1667391d4783cba15f217862506d9b05df0295fc
Christopher Burns <burnedchris+github@gmail.com>
2021-09-28 11:32:18 +0000

    removed rollup in favour of tsdx + added css-in-js

---
+++ sdk/demo/index.js
@@ -0,0 +1,18 @@
+import everfund from "../dist";
+
+document.getElementById("donate-modal").addEventListener("click", () => {
+  console.log("launch Everfund Modal");
+  everfund.modal({
+    code: "demo",
+    onSuccess: function (onSuccessPayload) {
+      console.log(onSuccessPayload);
+    },
+    onFailure: function (error) {
+      console.log(error);
+    },
+    onClose: function () {
+      //
+      console.log("Closed Modal");
+    }
+  });
+});
--- sdk/src/modal.ts
+++ sdk/src/modal.ts
@@ -1,28 +1,28 @@
-import "core-js/features"
-import "core-js/features/array/fill"
-import "core-js/features/array/includes"
-import "core-js/features/promise"
-import elementClosest from "element-closest"
-import "element-remove"
-import styles from "./modal.module.css"
-import { ModalProps } from "./types"
+import 'core-js/features';
+import 'core-js/features/array/fill';
+import 'core-js/features/array/includes';
+import 'core-js/features/promise';
+import elementClosest from 'element-closest';
+import 'element-remove';
+import { keyframes, css } from '@stitches/core';
+import { ModalProps } from './types';
 
 export interface CustomWindow extends Window {
-  Everfund: EverfundClient
+  Everfund: EverfundClient;
 }
 
-declare let window: CustomWindow
+declare let window: CustomWindow;
 
 class EverfundClient {
-  private modalOpen: boolean = false
-  private onSuccess: ModalProps["onSuccess"] = () => {}
-  private onFailure: ModalProps["onFailure"] = () => {}
-  private onClose: ModalProps["onClose"] = () => {}
+  private modalOpen: boolean = false;
+  private onSuccess: ModalProps['onSuccess'] = () => {};
+  private onFailure: ModalProps['onFailure'] = () => {};
+  private onClose: ModalProps['onClose'] = () => {};
 
   constructor() {
-    this.setupButtonListeners()
+    this.setupButtonListeners();
 
-    elementClosest(window)
+    elementClosest(window);
   }
 
   public modal({
@@ -32,204 +32,268 @@ class EverfundClient {
     onFailure,
     onClose,
   }: ModalProps) {
-    this.setupIframeListeners()
-    if (onSuccess) this.onSuccess = onSuccess
-    if (onFailure) this.onFailure = onFailure
-    if (onClose) this.onClose = onClose
-    const origin = window.location.origin
+  
+    this.setupIframeListeners();
+    if (onSuccess) this.onSuccess = onSuccess;
+    if (onFailure) this.onFailure = onFailure;
+    if (onClose) this.onClose = onClose;
+    const origin = window.location.origin;
 
-    this.disableBodyScroll()
+    this.disableBodyScroll();
 
-    const modalFrame = document.createElement("iframe")
+    const modalFrame = document.createElement('iframe');
 
     let makeQS = function (obj: any) {
-      var str = []
+      var str = [];
       for (var p in obj)
         if (obj[p] && obj.hasOwnProperty(p)) {
-          str.push(encodeURIComponent(p) + "=" + encodeURIComponent(obj[p]))
+          str.push(encodeURIComponent(p) + '=' + encodeURIComponent(obj[p]));
         }
-      return str.join("&")
-    }
+      return str.join('&');
+    };
 
     modalFrame.src = `https://evr.fund/${code}/modal?${makeQS({
       embed_origin: origin,
       embeded: true,
       close_on_success: closeOnSuccess,
-    })}`
+    })}`;
 
     // @ts-ignore
-    modalFrame.allowPaymentRequest = true
-    modalFrame.className = styles.embedIframe
+    modalFrame.allowPaymentRequest = true;
 
-    modalFrame.addEventListener("load", function () {
+    const cssEmbedIframe = css({
+      border: 'none',
+      width: '100%',
+      margin: 0,
+      height: '100%',
+    });
+
+    modalFrame.className = cssEmbedIframe();
+
+    modalFrame.addEventListener('load', function () {
       const loadingSpinner = document.querySelector<HTMLDivElement>(
-        "." + styles.ldsRing
-      )
+        '.ldsRing'
+      );
       const modalWrap = document.querySelector<HTMLDivElement>(
-        "." + styles.embedModal
-      )
-      loadingSpinner?.remove()
-      modalWrap!.style.transform = "opacity(1)"
-    })
+        '.embedModal'
+      );
+      loadingSpinner?.remove();
+      modalWrap!.style.transform = 'opacity(1)';
+    });
 
-    const modalWrap = document.createElement("div")
-    modalWrap.className = styles.embedModal
-    modalWrap.appendChild(modalFrame)
+    const modalWrap = document.createElement('div');
 
-    const closebutton = document.createElement("button")
-    closebutton.className = styles.closeButton
+    const cssEmbedModal = css({
+      pointerEvents: 'all',
+      zIndex: '9999999',
+      display: 'flex',
+      width: '100%',
+      transform: 'opacity(0)',
+      transition: 'transform 0.3s ease',
+      overflowY: 'auto',
+      '-webkit-overflow-scrolling': 'touch',
+      height: '100%',
+    });
+
+    modalWrap.className = `embedModal ${cssEmbedModal()}`;
+    modalWrap.appendChild(modalFrame);
+
+    const closebutton = document.createElement('button');
+    closebutton.className = 'closeButton';
 
     closebutton.onclick = () => {
-      const embed = document.querySelector("." + styles.embedContainer)
-      embed && embed.remove()
-      Everfund.modalOpen = false
-      Everfund.onClose()
-      this.enableBodyScroll()
-    }
+      const embed = document.querySelector('#embedContainer');
+      embed && embed.remove();
+      Everfund.modalOpen = false;
+      Everfund.onClose();
+      this.enableBodyScroll();
+    };
 
-    const loadingSpinner = document.createElement("div")
-    loadingSpinner.className = styles.ldsRing
-    const div = document.createElement("div")
+    const loadingSpinner = document.createElement('div');
+
+    const cssKeyframeLDsring = keyframes({
+      '0%': { transform: 'rotate(0deg)' },
+      '100%': { transform: 'rotate(360deg)' },
+    });
+
+    const cssLdsRing = css({
+      display: "inline-block",
+      position: "absolute",
+      left: "calc(50% - 32px)",
+      top: "calc(50% - 32px)",
+      width: "64px",
+      height: "64px",
+      '& div': {
+        boxSizing: "border-box",
+        display: "block",
+        position: "absolute",
+        width: "51px",
+        height: "51px",
+        margin: "6px",
+        border: "6px solid white",
+        borderRadius: "50%",
+        animation: `${cssKeyframeLDsring} 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite`,
+        borderColor: "white transparent transparent transparent",
+      },
+      '& div:nth-child(1)': {
+        animationDelay:" -0.45s",
+      },
+      '& div:nth-child(2)': {
+        animationDelay:" -0.3s",
+      },
+      '& div:nth-child(3)': {
+        animationDelay:" -0.15s",
+      }
+    });
+    loadingSpinner.className = `ldsRing ${cssLdsRing()}` ;
+
+    const div = document.createElement('div');
 
     Array(4)
       .fill(4)
       .forEach(function () {
-        loadingSpinner.appendChild(div)
-      })
+        loadingSpinner.appendChild(div);
+      });
 
-    const embedContainer = document.createElement("div")
-    embedContainer.className = styles.embedContainer
-    embedContainer.appendChild(loadingSpinner)
-    embedContainer.appendChild(modalWrap)
+    const embedContainer = document.createElement('div');
 
-    document.body.appendChild(embedContainer)
+    const cssEmbedContainer = css({
+      position: 'fixed',
+      top: '0',
+      left: '0',
+      right: '0',
+      bottom: '0',
+      background: 'rgba(0, 0, 0, 0.7)',
+      zIndex: '9999998',
+      display: 'flex',
+      justifyContent: 'space-around',
+      alignItems: 'stretch',
+      backdropFilter: 'blur(8px)',
+    });
+
+    embedContainer.className = `embedContainer ${cssEmbedContainer()}`;
+    embedContainer.appendChild(loadingSpinner);
+    embedContainer.appendChild(modalWrap);
+
+    document.body.appendChild(embedContainer);
   }
 
   private enableBodyScroll(): void {
-    if (document.readyState === "complete") {
-      document.body.style.position = ""
-      document.body.style.overflowY = ""
-      document.body.style.width = ""
+    if (document.readyState === 'complete') {
+      document.body.style.position = '';
+      document.body.style.overflowY = '';
+      document.body.style.width = '';
 
       if (document.body.style.marginTop) {
-        const scrollTop = -parseInt(document.body.style.marginTop, 10)
-        document.body.style.marginTop = ""
-        window.scrollTo(window.pageXOffset, scrollTop)
+        const scrollTop = -parseInt(document.body.style.marginTop, 10);
+        document.body.style.marginTop = '';
+        window.scrollTo(window.pageXOffset, scrollTop);
       }
     } else {
-      window.addEventListener("load", this.enableBodyScroll)
+      window.addEventListener('load', this.enableBodyScroll);
     }
   }
 
   private disableBodyScroll({ savePosition = true } = {}): void {
-    if (document.readyState === "complete") {
+    if (document.readyState === 'complete') {
       if (document.body.scrollHeight > window.innerHeight) {
         if (savePosition)
-          document.body.style.marginTop = `-${window.pageYOffset}px`
-        document.body.style.position = "fixed"
-        document.body.style.overflowY = "scroll"
-        document.body.style.width = "100%"
+          document.body.style.marginTop = `-${window.pageYOffset}px`;
+        document.body.style.position = 'fixed';
+        document.body.style.overflowY = 'scroll';
+        document.body.style.width = '100%';
       }
     } else {
-      window.addEventListener("load", () =>
+      window.addEventListener('load', () =>
         this.disableBodyScroll({ savePosition })
-      )
+      );
     }
   }
 
   private setupButtonListeners() {
-    var head = document.head
-    var link = document.createElement("link")
-
-    link.type = "text/css"
-    link.rel = "stylesheet"
-    link.href = "https://script.everfund.co.uk/m.css"
-
-    head.appendChild(link)
-
     document.addEventListener(
-      "click",
+      'click',
       function (e: MouseEvent) {
-        const match = (e.target as HTMLElement).closest("[data-ef-modal]")
+        const match = (e.target as HTMLElement).closest('[data-ef-modal]');
 
-        if (!match || Everfund.modalOpen) return
-        e.preventDefault()
-        e.stopPropagation()
+        if (!match || Everfund.modalOpen) return;
+        e.preventDefault();
+        e.stopPropagation();
 
-        let code = match.getAttribute("data-ef-modal")
+        let code = match.getAttribute('data-ef-modal');
 
         if (!code) {
           console.error(
             'Everfund: data-ef-modal is required! eg <button data-ef-modal="demo"> modal </button>'
-          )
-          return
+          );
+          return;
         }
 
         if (
           !!new RegExp(
-            "^(https?:\\/\\/)?" + // protocol
-              "((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|" + // domain name
-              "((\\d{1,3}\\.){3}\\d{1,3}))" + // OR ip (v4) address
-              "(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*" + // port and path
-              "(\\?[;&a-z\\d%_.~+=-]*)?" + // query string
-              "(\\#[-a-z\\d_]*)?$",
-            "i"
+            '^(https?:\\/\\/)?' + // protocol
+              '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.)+[a-z]{2,}|' + // domain name
+              '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
+              '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
+              '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
+              '(\\#[-a-z\\d_]*)?$',
+            'i'
           ).test(code)
         ) {
           console.warn(
             `Everfund: url's are deprecated please use a code instead`
-          )
+          );
 
-          code = new URL(code).pathname.replace("/", "")
+          code = new URL(code).pathname.replace('/', '');
         }
 
-        Everfund.modalOpen = true
+        Everfund.modalOpen = true;
         Everfund.modal({
           code,
           onSuccess: () => {},
           onFailure: () => {},
           onClose: () => {},
-        })
+        });
       },
       false
-    )
+    );
   }
 
   private setupIframeListeners() {
     window.addEventListener(
-      "message",
+      'message',
       function (e) {
         switch (e.data.message) {
-          case "everfund:ready":
+          case 'everfund:ready':
             const loadingSpinner = document.querySelector<HTMLDivElement>(
-              "." + styles.ldsRing
-            )
+              '#ldsRing'
+            );
             const modalWrap = document.querySelector<HTMLDivElement>(
-              "." + styles.embedModal
-            )
-            loadingSpinner?.remove()
-            modalWrap!.style.transform = "opacity(1)"
-            break
-          case "everfund:success":
-            const data = e.data.content
-            Everfund.onSuccess(data)
-            break
-          case "everfund:failure":
-            Everfund.onFailure(e.data.content)
-            break
-          case "everfund:close":
-            const embed = document.querySelector("." + styles.embedContainer)
-            embed && embed.remove()
-            Everfund.modalOpen = false
-            Everfund.onClose()
-            break
+              '.' + 'embedModal'
+            );
+            loadingSpinner?.remove();
+            modalWrap!.style.transform = 'opacity(1)';
+            break;
+          case 'everfund:success':
+            const data = e.data.content;
+            Everfund.onSuccess(data);
+            break;
+          case 'everfund:failure':
+            Everfund.onFailure(e.data.content);
+            break;
+          case 'everfund:close':
+            const embed = document.querySelector('.' + 'embedContainer');
+            embed && embed.remove();
+            Everfund.modalOpen = false;
+            Everfund.onClose();
+            break;
         }
       },
       false
-    )
+    );
   }
 }
 
-const Everfund = new EverfundClient()
+const Everfund = new EverfundClient();
 
-export default Everfund
+export default Everfund;
