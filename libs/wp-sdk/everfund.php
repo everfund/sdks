7c6a3c6256c05360e9e91716c68455c26bf3a78e
Christopher Burns <christopher@everfund.io>
2022-05-20 21:15:03 +0100

    Migrated WP-SDK to monorepo

---
+++ libs/wp-sdk/everfund.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ * Plugin Name: Everfund
+ * Plugin URI: https://everfund.io
+ * Description: Everfund takes the friction out of donating with an easy to set up donation portal for your WordPress website that help you convert more income for your charity and give your donors the best experience possible.
+ * Tags: donation, donations, nonprofit, nonprofits, charity, charities, donation widget, fundraising, payment, payments, crowdfunding, campaign, stripe, campaigns, social causes, causes, credit card, credit cards, bacs, direct-debits
+ * Requires at least: 5.3
+ * Requires PHP: 7.0
+ * Version: 1.3.3
+ * Author: Everfund
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       everfund.io
+ */
+function everfund_show_apple_pay_domain_verification_file()
+{
+    if ($_SERVER['REQUEST_URI'] == '/.well-known/apple-developer-merchantid-domain-association') {
+        readfile(dirname(__FILE__).'/apple-developer-merchantid-domain-association');
+        exit();
+    }
+}
+
+function everfund_create_menu()
+{
+    add_menu_page(__('Donations', 'everfund'), __('Donations', 'everfund'), 'administrator', __FILE__, 'everfund_plugin_settings_page', 'dashicons-info');
+    add_action('plugins_loaded', 'register_everfund_plugin_textdomain');
+}
+
+function everfund_admin_styles()
+{
+    wp_register_style('custom_wp_admin_css', plugins_url('css/style.css', __FILE__), false, '1.0.0');
+    wp_enqueue_style('custom_wp_admin_css');
+}
+
+function everfund_plugin_settings_page()
+{
+    ?>
+  <div class="wrap everfund-wrap">
+    <div class="everfund-modal">
+      <h2 class="everfund-title"><?php _e('Everfund is Installed.', 'everfund'); ?></h2>
+      <p class="everfund-subtitle"><?php _e('You can now use Everfund on your website.', 'everfund'); ?></p>
+      <a class="everfund-button everfund-neutral" href="https://dashboard.everfund.io/donations/"><?php _e('Go to my Widgets', 'everfund'); ?></a>
+      <a class="everfund-button everfund" href="https://dashboard.everfund.io/donations/links/"><?php _e('Go to my Donations', 'everfund'); ?></a>
+	  <p class="everfund-stuck"><?php _e('Stuck? Talk to ', 'everfund'); ?> <a href="https://help.everfund.co.uk/en/"> Support </a> <?php _e(' or check out the ', 'everfund'); ?><a href="https://developer.everfund.co.uk/integrations/wordpress">developer documentation </a></p>
+    </div>
+  </div>
+
+  <?php
+}
+
+function everfund_sdk_script()
+{
+    wp_enqueue_script('everfund', plugins_url('/js/js-sdk.umd.js', __FILE__));
+}
+
+function add_allowed_origins($origins)
+{
+    $origins[] = 'https://evr.fund';
+    $origins[] = 'https://animals.charity';
+    $origins[] = 'https://appeal.charity';
+    $origins[] = 'https://emergency.charity';
+    $origins[] = 'https://giveto.charity';
+    $origins[] = 'https://hospice.charity';
+    $origins[] = 'https://nhs.charity';
+    $origins[] = 'https://shelter.charity';
+    $origins[] = 'https://urgent.charity';
+    $origins[] = 'https://everfund.co.uk';
+    $origins[] = 'https://api.everfund.co.uk';
+    $origins[] = 'https://everfund.io';
+    $origins[] = 'https://api.everfund.io';
+
+    return $origins;
+}
+
+add_filter('allowed_http_origins', 'add_allowed_origins');
+add_action('admin_enqueue_scripts', 'everfund_admin_styles');
+add_action('wp_enqueue_scripts', 'everfund_sdk_script');
+add_action('wp_head', 'everfund_sdk_script', 1);
+add_action('plugins_loaded', 'everfund_show_apple_pay_domain_verification_file');
+add_action('admin_menu', 'everfund_create_menu');
---
+++ libs/wp-sdk/readme.txt
@@ -0,0 +1,69 @@
+=== Everfund Donation Widget ===
+Contributors: everfund.io
+Donate link: https://everfund.io
+Tags:  donation, donations, nonprofit, nonprofits, charity, charities, donation widget, fundraising, payment, payments, crowdfunding, campaign, stripe, campaigns, social causes, causes, credit card, credit cards, bacs, direct-debits
+Requires at least: 4.5.0
+Tested up to: 5.8.1
+Requires PHP: 5.3
+Stable tag: 1.3.3
+License: MIT
+License URI: https://opensource.org/licenses/MIT
+
+Everfund takes the friction out of donating with an easy to set up donation portal for your WordPress website that help you convert more income for your charity and give your donors the best experience possible.
+
+== Description ==
+
+### The best donor experience*
+
+At Everfund, we are pinoeers in donation technology and believe that nothing should get in the way of a donor wanting to support you. Everfund ensures your WordPress website has a high performance donation portal which helps donors make a full and frictionless donation in 30 seconds or less without ever leaving your site.
+
+We have the best donor experience for the highest donation conversions. 92% of donors cover the donation fees on your behalf and on average costs 6.5x less than using platforms like JustGiving for donations.
+
+### Features for high performance donations
+
+Everfund manages everything you need to take a donation, not just a payment. Make your website into a high performance fundraising machine with all the features you need to take a full donation for modern donors:
+
+1. Single Donations
+2. Recurring Donations
+3. Gift Aid
+4. GDPR Concent
+5. Cover Processor Fees
+6. Donation Emails
+7. Customisable Donation System
+8. Apple / Google Pay
+
+### Secure, low admin time and always up to date
+
+Everfund keeps everything secure and updated without having to worry about updating plugins when changes are made and leaves you to focus on your fundraising.
+
+### Words from a happy charities on Everfund 
+
+**Rachel, Trustee @ Society for Renaissance Studies**
+
+"I'm glad we don't have to use PayPal Donate, Everfund's platform is solving a problem we thought was insurmountable"
+
+**Simon Hoare, CEO @ Acts Trust**
+
+"Everfund has fundamentally changed our fundraising, it's given us a easy-to-use platform and most importantly will work for our donors, thousands are raised every month and the significant increase of donations have helped us expand our services despite of Coronavirus"
+
+== Installation ==
+
+Upload the plugin files to the `/wp-content/plugins/everfund-sdk` directory, or install the plugin through the WordPress plugins screen directly.
+Once uploaded using the editor add the everfund code to a button or add html code? 
+
+Stuck? Check out our developer documentation [here](https://developer.everfund.io/donation-button)
+
+== Disclosure ==
+
+Everfund Wordpress Plugin will automatically add the Everfund’s SDK to your header. It is hosted on [JSDeliver](https://www.jsdelivr.com) as a CDN Version of our [NPM Package](https://www.npmjs.com/package/@everfund/sdk)
+Here are links to view JSDelivr's Policy's, [Acceptable Use Policy](https://www.jsdelivr.com/terms/acceptable-use-policy-jsdelivr-net) and [Privacy Policy](https://www.jsdelivr.com/terms/privacy-policy-jsdelivr-net).
+
+== Screenshots ==
+
+1. Everfund Donation Widget on Website
+2. Everfund Dashboard
+
+== Changelog ==
+
+= 1.1.0 =
+* Initial Release
