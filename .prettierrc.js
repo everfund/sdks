b8147fcfa5546468c43f8cc1948c14068bc03e1b
burnedchris <christopher@everfund.com>
2023-08-07 13:42:35 +0100

    style: Removed unneeded comment

--- .prettierrc.js
+++ .prettierrc.js
@@ -3,5 +3,4 @@
  */
 module.exports = {
   ...require('@everfund/prettier/.prettierrc.json'),
-  // proseWrap: 'always', // Uncomment this while working on Markdown documents. MAKE SURE TO COMMENT THIS BEFORE RUNNING CHECKS/FORMATS OR EVERYTHING WILL BE MODIFIED.
 };
