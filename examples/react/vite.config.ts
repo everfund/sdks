4390b88818f8933e75282c75dd14b62c7060132b
Simon Grimshaw <simon@everfund.com>
2023-08-09 16:54:27 +0100

    Removing console.log

--- examples/react/vite.config.ts
+++ examples/react/vite.config.ts
@@ -3,8 +3,6 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import * as path from 'path';
 
-console.log(process.env.USE_SOURCE, process.env.USE_SOURCE === 'true');
-
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
