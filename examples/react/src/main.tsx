e4b9754810deb59fb3a3cabf803cf40e9abfda47
burnedchris <christopher@everfund.com>
2023-08-30 11:21:13 -0700

    fix: :fire: fixes style files

--- examples/react/src/main.tsx
+++ examples/react/src/main.tsx
@@ -1,7 +1,7 @@
 import { useDonationCheckoutModal, DonationForm } from '@everfund/react-sdk';
 import ReactDOM from 'react-dom/client';
 import * as React from 'react';
-import './styles/styles.css';
+import './demo.css';
 
 const code = 'j1q16MvJl5lqfji06bVFKei3HZkW';
 
